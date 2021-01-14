1361a3b2d697b075e7880fde80247ae3e35a634e
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2021-01-14 21:12:03 +0100

    minor

--- src/TaskBasedUpdater/New/Update/Service/UpdaterEngine.cs
+++ src/TaskBasedUpdater/New/Update/Service/UpdaterEngine.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using TaskBasedUpdater.Configuration;
 using Validation;
 
@@ -10,6 +11,8 @@ namespace TaskBasedUpdater.New.Update.Service
     internal class UpdaterEngine
     {
         private readonly IServiceProvider _serviceProvider;
+        private ILogger? _logger;
+        private bool _isInitialized;
 
         public UpdateConfiguration UpdateConfiguration { get; }
 
@@ -33,10 +36,35 @@ namespace TaskBasedUpdater.New.Update.Service
         
         public void Initialize()
         {
+            if (_isInitialized)
+                return;
+            if (IsDisposed)
+                throw new ObjectDisposedException(nameof(UpdaterEngine));
+
+            _logger = _serviceProvider.GetService<ILogger>();
+
+
+            _isInitialized = true;
         }
 
-        public void Update(IUpdateCatalog updateCatalog, CancellationToken cancellation)
+        public void Update(IUpdateCatalog updateCatalog, CancellationToken cancellation = default)
         {
+            Requires.NotNull(updateCatalog, nameof(updateCatalog));
+            Initialize();
+            try
+            {
+
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError(ex, ex.Message);
+                throw;
+            }
+
             Task.Run(async () => await Task.Delay(10000, cancellation), cancellation).Wait(cancellation);
         }
 
@@ -44,6 +72,7 @@ namespace TaskBasedUpdater.New.Update.Service
         {
             if (IsDisposed)
                 return;
+
 
             IsDisposed = true;
         }
