821438191a2d8b7f41adc046319a9996e76a5702
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2021-02-20 11:21:15 +0100

    nullable

--- src/ProductMetadata/Services/IVariableResolver.cs
+++ src/ProductMetadata/Services/IVariableResolver.cs
@@ -4,14 +4,14 @@ namespace ProductMetadata.Services
 {
     public interface IVariableResolver
     {
-        string ResolveVariables(string value, IDictionary<string, string> variables);
+        string ResolveVariables(string value, IDictionary<string, string?> variables);
     }
 
     public class VariableResolver : IVariableResolver
     {
         public static readonly IVariableResolver Default = new VariableResolver();
 
-        public string ResolveVariables(string value, IDictionary<string, string> variables)
+        public string ResolveVariables(string value, IDictionary<string, string?> variables)
         {
             throw new System.NotImplementedException();
         }
